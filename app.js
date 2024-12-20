3fb9a8256ee40bc9d616746fe9391dd488a3f7e5
Zahra Karandish <zkarandish@gmail.com>
2024-12-19 19:53:28 -0500

    redirect

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const fs= require('fs');
 
 // function rqListener(req, res){
 // }
 
 const server= http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    const url = req.url;
+    const method = req.method;
+    if(url ==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+       return res.end();
+    }
+    if(url ==='/message'&& method === 'POST'){
+        fs.writeFileSync('message.txt','Dummy');
+        res.statusCode=302;
+        res.setHeader('Location','/');
+         return res.end();
+
+
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>my first page</title></head>');
