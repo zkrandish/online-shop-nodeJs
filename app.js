b60d48b65cecc1efa64d8775a0d542ed37851a3d
Zahra Karandish <zkarandish@gmail.com>
2024-12-23 11:53:27 -0500

    404 page not found added

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const bodyParser= require('body-parser');
 const app = express();
 
 const adminRoutes= require('./routes/admin');
-
 const shopRoutes= require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
 
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h2>')
+});
 
 
 
