b1d695a4fd91c71565519ff377ce851e5c3e3598
Zahra Karandish <zkarandish@gmail.com>
2024-12-23 10:04:40 -0500

    basic response

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require('express');
 
 const app = express();
 
+//add a middleware function
+app.use((req, res, next)=>{
+    console.log('in middleware');
+    next(); //allows the requiest to continue to the next middleware in line
+});
+
+app.use((req, res, next)=>{
+    console.log('in another middleware');
+    res.send('<h1>heloo express</h1>');
+});
+
+
 const server= http.createServer(app);
 
 server.listen(3000);
