2078f3989b648199de866b10eb4eccfc42298897
Zahra Karandish <zkarandish@gmail.com>
2024-12-24 16:15:27 -0500

    adding handlebars

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const path = require('path');
 const express = require('express');
 const bodyParser= require('body-parser');
-
+const expressHbs= require('express-handlebars');
 const app = express();
+
+app.engine(
+    'hbs',
+    expressHbs({
+      layoutsDir: 'views/layouts/',
+      defaultLayout: 'main-layout',
+      extname: 'hbs'
+    })
+  );
+     
+app.set('view engine', 'hbs');
+app.set('views','views');
 
 const adminData= require('./routes/admin');
 const shopRoutes= require('./routes/shop');
@@ -14,7 +26,8 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{pageTilte: 'Page Not Found'})
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,17 @@ const router = express.Router();
 
 
 router.get('/',(req, res, next)=>{
-    console.log('shop.js',adminData.products);
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    // console.log('shop.js',adminData.products);
+    // res.sendFile(path.join(rootDir,'views','shop.html'));
+    const products = adminData.products;
+    res.render('shop',{
+        prods:products, 
+        pageTitle: 'shop', 
+        path:'/', 
+        hasProducts: products.length>0,
+        activeShop:true,
+        productCSS: true
+    })
 });
 
 module.exports= router;
