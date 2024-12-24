d9ea212d4f0d8142e5ab0cae74621b521cf09728
Zahra Karandish <zkarandish@gmail.com>
2024-12-24 16:59:21 -0500

    ejs done

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser= require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData= require('./routes/admin');
 const shopRoutes= require('./routes/shop');
 
@@ -13,11 +16,8 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
-
-
-
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' , path:''});
+  });
 
 app.listen(3000);
