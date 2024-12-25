80852ea6e12aef8fa04902676083605858a1c755
Zahra Karandish <zkarandish@gmail.com>
2024-12-25 11:52:02 -0500

    save and fetch from file

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,30 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next)=>{
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-      });
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 };
 
-exports.postAddProduct = (req, res, next)=>{
-    const product = new Product(req.body.title);
-    product.save();
-     res.redirect('/');
- };
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
- exports.getProducts = (req, res, next)=>{
-    const products= Product.fetchAll();
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
     res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
+  });
 };
--- models/product.js
+++ models/product.js
@@ -1,14 +1,42 @@
-const products =[];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product {
-    constructor(t){
-        this.title = t;
-    }
-    save(){
-        products.push(this);
-    }
-    static fetchAll(){
-         return products;
-    }
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
 
+const getProductsFromFile = (cb)=> {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        try {
+            cb(JSON.parse(fileContent));
+        } catch {
+            cb([]);
+        }
+    });
 }
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products =>{
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+            if (err) {
+                console.error("Error writing to file:", err);
+            } 
+        });
+    });
+}
+
+static fetchAll(cb) {
+    getProductsFromFile(cb);
+}
+
+};
