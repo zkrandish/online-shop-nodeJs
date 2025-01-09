4b935afe3a2c5834b2b95799c8d9d334c6e39118
Zahra Karandish <zkarandish@gmail.com>
2025-01-09 06:31:37 -0500

    get admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -60,13 +60,15 @@ exports.getAddProduct = (req, res, next) => {
   };
 
   exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
+    Product.findAll()
+    .then(products => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products'
       });
-    });
+    })
+    .catch(err=> console.log(err));
   };
 
   exports.postDeleteProduct=(req,res,next)=>{
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { where } = require('sequelize');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll().then(products =>{
@@ -16,16 +17,26 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct= (req,res,next)=>{
   const prodId = req.params.productId;
-  Product.findById(prodId, product=>{
+  // Product.findAll({where: {id: prodId}})
+  // .then(products=>{
+  //   res.render('shop/product-details', {
+  //     product: products[0],
+  //     pageTitle: products[0].title,
+  //     path: '/products'
+  //   });
+  // }).catch(err=>{console.log(err)});
+  Product.findByPk(prodId)
+  .then(product=>{
     res.render('shop/product-details', {
       product: product,
       pageTitle: product.title,
       path: '/products'
-
     });
+  })
+  .catch(err=>{
+    console.log(err);
   });
- 
-}
+};
 
 exports.getIndex= (req, res, next)=>{
   Product.findAll().then(products =>{
