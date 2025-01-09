a862cd09d7a30bb3a10a6466f653337a4e6e4328
Zahra Karandish <zkarandish@gmail.com>
2025-01-09 06:04:33 -0500

    sync models to the databse

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const bodyParser= require('body-parser');
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -19,4 +20,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result=>{
+    //console.log(result);
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+});
+
