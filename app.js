f5e55b473255502860b842d414f49b8a5a9a0a55
Zahra Karandish <zkarandish@gmail.com>
2025-01-10 06:15:22 -0500

    acceing the cart

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart,{through: CartItem});
 
 sequelize
-.sync({ force: true})
-//.sync()
+//.sync({ force: true})
+.sync()
 .then(result=>{
    return User.findByPk(1);
     //console.log(result);
@@ -58,6 +58,9 @@ sequelize
 })
 .then(user=>{
     //console.log(user);
+   return user.createCart();
+})
+.then(cart=>{
     app.listen(3000);
 })
 .catch(err=>{
