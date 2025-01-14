d4594d26ccc0c5b8330da4568e04046fc49d8926
Zahra Karandish <zkarandish@gmail.com>
2025-01-14 07:34:58 -0500

    edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -27,36 +30,40 @@ exports.getAddProduct = (req, res, next) => {
       });
   };
   
-  // exports.getEditProduct = (req, res, next) => {
-  //   const editMode = req.query.edit;
-  //   if(!editMode){
-  //    return res.redirect('/');
-  //   }
-  //   const prodId= req.params.productId;
-  //   Product.findById(prodId, product=>{
-  //     if(!product){
-  //       return res.redirect('/');
-  //     }
-  //     res.render('admin/edit-product', {
-  //       pageTitle: 'Edit Product',
-  //       path: '/admin/edit-product',
-  //       editing: editMode,
-  //       product: product
-  //     });
-  //   });
-  // };
+  exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+     return res.redirect('/');
+    }
+    const prodId= req.params.productId;
+    Product.findById(prodId)
+    .then(product=>{
+      if(!product){
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
+  };
 
-  // exports.postEditProduct=(req,res,next)=>{
-  //   const prodId= req.body.productId;
-  //   const updatedTilte= req.body.title;
-  //   const updatedPrice = req.body.price;
-  //   const updatedImageUrl = req.body.imageUrl;
-  //   const updatedDesc= req.body.description;
-  //   const updatedProduct = new Product(prodId, updatedTilte,updatedImageUrl, updatedDesc,updatedPrice);
-  //   updatedProduct.save();
-  //   res.redirect('/admin/products');
-
-  // };
+  exports.postEditProduct=(req,res,next)=>{
+    const prodId= req.body.productId;
+    const updatedTilte= req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc= req.body.description;
+    const updatedProduct = new Product( updatedTilte,updatedPrice, updatedDesc,updatedImageUrl, new ObjectId(prodId));
+    updatedProduct.save()
+    .then(result =>{
+      res.redirect('/admin/products');
+    })
+    .catch(err=> console.log(err));
+  };
 
   exports.getProducts = (req, res, next) => {
     Product.fetchAll()
