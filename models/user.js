8bfac0abff9b83d936ffd69268d7db1403f57da3
Zahra Karandish <zkarandish@gmail.com>
2025-01-15 06:18:14 -0500

    add user in mongo

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongodb = require('mongodb');
+const { getDb } = require('../util/database');
+
+const ObjectId = mongodb.ObjectId;
+
+const db = require('../util/database').getDb;
+
+class User{
+    constructor(username, email){
+        this.name = username;
+        this.email = email;
+    }
+
+    save(){
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId){
+        const db = getDb();
+        return db.collection('users')
+        .findOne({_id: new ObjectId(userId)});
+    }
+}
+
+module.exports = User;
