a217d9f54ea81b661cdc24f84166d42a9949de39
Zahra Karandish <zkarandish@gmail.com>
2025-01-15 06:59:37 -0500

    add to cart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('678798ed7d11d3e6ff9a27b1')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -6,14 +6,29 @@ const ObjectId = mongodb.ObjectId;
 const db = require('../util/database').getDb;
 
 class User{
-    constructor(username, email){
+    constructor(username, email, cart, id){
         this.name = username;
         this.email = email;
+        this.cart = cart; //{items:[]}
+        this._id = id;
     }
 
     save(){
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product){
+        // const cartProducts = this.cart.items.findIndex(cp =>{
+        //     return cp._id === product._id;
+        // });
+    
+        const updatedCart = {items: [{productId: new ObjectId(product._id), quantity: 1}]};
+        const db = getDb();
+        return db.collection('users')
+        .updateOne({_id: new ObjectId(this._id)},
+        {$set: {cart: updatedCart}}
+    );
     }
 
     static findById(userId){
