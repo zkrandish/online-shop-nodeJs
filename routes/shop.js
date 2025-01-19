dbfa8efdd3ef086d02e8800759f0e90bbf220c99
Zahra Karandish <zkarandish@gmail.com>
2025-01-19 10:50:20 -0500

    get products

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,9 @@ const shopController = require('../controllers/shop')
 const router = express.Router();
 
 
-// router.get('/',shopController.getIndex);
+router.get('/',shopController.getIndex);
 
-// router.get('/products',shopController.getProducts);
+router.get('/products',shopController.getProducts);
 
 // router.get('/products/:productId', shopController.getProduct);
 
