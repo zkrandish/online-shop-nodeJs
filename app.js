b486839209f4be937286dd57de803ec983651077
Zahra Karandish <zkarandish@gmail.com>
2025-01-19 10:18:16 -0500

    mongoose set up connection

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +32,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+.connect('mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(result=>{
   app.listen(3000);
-});
+})
+.catch(err=>{
+  console.log(err);
+})
--- routes/admin.js
+++ routes/admin.js
@@ -5,17 +5,17 @@ const { title } = require('process');
 
 const router = express.Router();
 
-// /admin/add-product =>GET
-router.get('/add-product',adminController.getAddProduct);
+// // /admin/add-product =>GET
+// router.get('/add-product',adminController.getAddProduct);
 
-router.get('/products', adminController.getProducts);
+// router.get('/products', adminController.getProducts);
 
-router.post('/add-product',adminController.postAddProduct);
+// router.post('/add-product',adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+// router.get('/edit-product/:productId', adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+// router.post('/edit-product', adminController.postEditProduct);
 
-router.post('/delete-product',adminController.postDeleteProduct);
+// router.post('/delete-product',adminController.postDeleteProduct);
 
 module.exports = router;
