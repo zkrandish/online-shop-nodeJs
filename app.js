5c0726735b11cf5a69802eaebcc58cc1ddf29995
Zahra Karandish <zkarandish@gmail.com>
2025-01-20 15:01:40 -0500

    add csrf protection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -28,7 +31,16 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret:'my secret',resave:false, saveUninitialized:false, store:store }));
+app.use(
+  session({
+    secret:'my secret',
+    resave:false,
+     saveUninitialized:false, 
+     store:store 
+    })
+  );
+
+app.use(csrfProtection);
 
 app.use((req,res,next)=>{
   if(!req.session.user){
@@ -40,7 +52,12 @@ app.use((req,res,next)=>{
       next();
     })
     .catch(err => console.log(err));
+});
 
+app.use((req,res,next)=>{
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -36,8 +36,7 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/',
-        isAuthenticated: req.session.isLoggedIn
+        path: '/'
       });
     })
     .catch(err => {
@@ -91,7 +90,7 @@ exports.postOrder = (req, res, next) => {
     });
     const order = new Order({
       user: {
-        name: req.user.name,
+        email: req.user.email,
         userId: req.user
       },
       products: products
