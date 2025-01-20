8c4f784842b8adb0188a5372159a8e446a0018d8
Zahra Karandish <zkarandish@gmail.com>
2025-01-20 10:16:12 -0500

    update the views and sessions

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
+
 const app = express();
+
+const store = new MongoDBStore({
+  uri:MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,16 +27,23 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'my secret',resave:false, saveUninitialized:false}))
 
-app.use((req, res, next) => {
-  User.findById('678d27bde1622416419ec4a9')
+app.use(session({secret:'my secret',resave:false, saveUninitialized:false, store:store }));
+
+app.use((req,res,next)=>{
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
+
 });
+
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +52,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect('mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+.connect(MONGODB_URI)
 .then(result=>{
   User.findOne()
   .then(user=>{
--- controllers/admin.js
+++ controllers/admin.js
@@ -5,8 +5,7 @@ exports.getAddProduct = (req, res, next) => {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
       editing: false,
-      isAuthenticated: req.isLoggedIn
-      
+      isAuthenticated: req.session.isLoggedIn
     });
   };
   
@@ -44,7 +43,7 @@ exports.getAddProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -80,7 +79,7 @@ exports.getAddProduct = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err=> console.log(err));
