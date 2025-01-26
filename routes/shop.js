4fc0dbd03c5c21a31852109adf5aa92f817a3f68
Zahra Karandish <zkarandish@gmail.com>
2025-01-26 15:53:56 -0500

    add authorization to download

--- routes/shop.js
+++ routes/shop.js
@@ -22,6 +22,6 @@ router.post('/create-order',isAuth,shopController.postOrder);
 
 router.get('/orders',isAuth,shopController.getOrders);
 
-router.get('/orders/:orderId',isAuth, shopController.getInvoice);
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
 module.exports= router;
