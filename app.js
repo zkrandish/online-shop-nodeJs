10bfec0a4b7c417b3e35caee90b6e0ed79cad5cf
Zahra Karandish <zkarandish@gmail.com>
2025-01-26 11:54:38 -0500

    still error

--- app.js
+++ app.js
@@ -15,27 +15,29 @@ const User = require('./models/user');
 const MONGODB_URI = 'mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
 
 const app = express();
-
 const store = new MongoDBStore({
-  uri:MONGODB_URI,
+  uri: MONGODB_URI,
   collection: 'sessions'
 });
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-  destination:(req, file, cb)=>{
-    cb(null, 'images')
+  destination: (req, file, cb) => {
+    cb(null, 'images');
   },
-  filename:(req, file, cb)=>{
-    cb(null,new Date().toISOString() + '-' + file.originalname);
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
 
-const fileFilter = (req, file, cb) =>{
-  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
-
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
     cb(null, true);
-  } else{
+  } else {
     cb(null, false);
   }
 };
@@ -48,37 +50,36 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage, fileFilter:fileFilter}).single('image'));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images',express.static(path.join(__dirname, 'images')));
-
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
-    secret:'my secret',
-    resave:false,
-     saveUninitialized:false, 
-     store:store 
-    })
-  );
-
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 app.use(csrfProtection);
 app.use(flash());
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
-
-app.use((req,res,next)=>{
-  if(!req.session.user){
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
-      if(!user){
+      if (!user) {
         return next();
       }
       req.user = user;
@@ -86,7 +87,7 @@ app.use((req,res,next)=>{
     })
     .catch(err => {
       next(new Error(err));
-      });
+    });
 });
 
 app.use('/admin', adminRoutes);
@@ -97,21 +98,21 @@ app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-app.use((error, req,res,next)=>{
-  //res.status(error.httpStatusCode).render(...);
-  //res.redirect('/500');
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
   res.status(500).render('500', {
-     pageTitle: 'Error'
-     ,path:'/500'
-     ,isAuthenticated: req.session.isLoggedIn
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
   });
 });
 
 mongoose
-.connect(MONGODB_URI)
-.then(result=>{
-  app.listen(3000);
-})
-.catch(err=>{
-  console.log(err);
-}) 
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- routes/admin.js
+++ routes/admin.js
@@ -1,42 +1,54 @@
 const path = require('path');
+
 const express = require('express');
-const {body} = require('express-validator');
-const adminController = require('../controllers/admin')
+const { body } = require('express-validator');
+
+const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
-// // /admin/add-product =>GET
-router.get('/add-product',isAuth , adminController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
-router.get('/products',isAuth , adminController.getProducts);
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product',[
+// /admin/add-product => POST
+router.post(
+  '/add-product',
+  [
     body('title')
-    .isString()
-    .isLength({min:3})
-    .trim(),
-    body('price')
-    .isNumeric()
-    .isFloat(),
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
     body('description')
-    .isLength({min:5, max:400})
-    .trim()
-],
-isAuth ,adminController.postAddProduct);
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
-router.get('/edit-product/:productId',isAuth , adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product',[
+router.post(
+  '/edit-product',
+  [
     body('title')
-    .isString()
-    .isLength({min:3})
-    .trim(),
-    body('price').isNumeric().isFloat(),
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
     body('description')
-    .isLength({min:5, max:400})
-    .trim()
-], isAuth ,adminController.postEditProduct);
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
-router.post('/delete-product',isAuth ,adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
