9955ae3465742fe2113b547e6dbf6c815bcc802c
Zahra Karandish <zkarandish@gmail.com>
2025-01-28 07:33:30 -0500

    async requestes

--- controllers/admin.js
+++ controllers/admin.js
@@ -185,8 +185,8 @@ exports.getAddProduct = (req, res, next) => {
    
   };
 
-  exports.postDeleteProduct=(req,res,next)=>{
-    const prodId = req.body.productId;
+  exports.deleteProduct=(req,res,next)=>{
+    const prodId = req.params.productId;
     Product.findById(prodId)
     .then(product=>{
       if(!product){
@@ -197,11 +197,9 @@ exports.getAddProduct = (req, res, next) => {
     })
     .then(()=>{
       console.log('destroyed product');
-      res.redirect('/admin/products');
+      res.status(200).json({message:'Success!'});
     })
     .catch(err=>{
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message: 'Deleting product failed.!'});
     })
   }
